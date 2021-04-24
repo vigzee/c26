e499f75f08887959b9d64bad34608825a4029f6c
vigzee <vzeema15@gmail.com>
2021-04-24 15:43:43 +0530

    did nothing

--- sketch.js
+++ sketch.js
@@ -12,7 +12,7 @@ function setup(){
 
     box1 = new Box(200,300,50,50);
     box2 = new Box(240,100,50,100);
-    ground = new Ground(200,height,400,20)
+    ground = new Ground(200,390,400,20)
 }
 
 function draw(){
